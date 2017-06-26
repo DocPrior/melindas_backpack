7b08d5260d9bc3cfed94bfb2b9854f25b51976b3
DocPrior <Sara Prior>
2017-06-26 14:58:50 -0400

    fix smell 4

--- backpack.rb
+++ backpack.rb
@@ -13,28 +13,55 @@ class Backpack
     # set up local variables used by rest of prepare method
     weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
-
     # Ensure appropriate clothing is added to backpack
-    if weather == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'umbrella'
-    elsif weather == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'jacket'
-    else
-      @items << 'pants'
-      @items << 'shirt'
-    end
-
+    pack_by_weather
     # Ensure gym shoes are added to backpack if it's a gym day
+    gym_shoes
+    # Bring a packed lunch on all weekdays
+    pack_lunch
+  end
+
+  def pack_by_weather
+    # set up local variables used by rest of prepare method
+    weather = @attributes[:weather]
+    day_of_week = @attributes[:day_of_week]
+    if weather == 'rainy'
+      rainy_weather
+    elsif weather == 'cold'
+      cold_weather
+    else
+      other_weather
+    end
+  end
+
+  def rainy_weather
+    @items << 'pants'
+    @items << 'shirt'
+    @items << 'umbrella'
+  end
+
+  def cold_weather
+    @items << 'pants'
+    @items << 'shirt'
+    @items << 'jacket'
+  end
+
+  def other_weather
+    @items << 'pants'
+    @items << 'shirt'
+  end
+
+  def gym_shoes
+    weather = @attributes[:weather]
+    day_of_week = @attributes[:day_of_week]
     if day_of_week == 'monday' || day_of_week == 'thursday'
-      #add gym shoes to items
       @items << 'gym shoes'
     end
+  end
 
-    # Bring a packed lunch on all weekdays
+  def pack_lunch
+    weather = @attributes[:weather]
+    day_of_week = @attributes[:day_of_week]
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
